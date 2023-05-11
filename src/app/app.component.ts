9a7d830198dc94969493d4d30ad098703e8fe41c
Davood <da.mirzaee@gmail.com>
2023-05-11 14:26:39 +0330

    We made a pipe. A pipe full of nonscence.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'pipes';
+  name!: string;
+  date!: string;
+  amount!: number;
+  onNameChange(value: string) {
+    this.name = value;
+  }
+  onDateChange(value: string) {
+    this.date = value;
+  }
+  onAmountChange(value: number) {
+    this.amount = value;
+  }
 }
