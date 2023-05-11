18697b602186ee30fc952f05d75c96afa0ee612e
Davood <da.mirzaee@gmail.com>
2023-05-11 15:44:52 +0330

    Is this our final or we solve the result

--- src/app/convert.pipe.ts
+++ src/app/convert.pipe.ts
@@ -10,11 +10,11 @@ export class ConvertPipe implements PipeTransform {
     }
     switch (units) {
       case 'km':
-        return value * 1.6;
+        return value * 1.609344;
       case 'm':
-        return value * 1.6 * 1000;
+        return value * 1.609344 * 1000;
       case 'cm':
-        return value * 1.6 * 1000 * 100;
+        return value * 1.609344 * 1000 * 100;
       default:
         throw new Error('not a supported type');
     }
